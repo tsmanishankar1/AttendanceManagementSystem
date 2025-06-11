eccf681d35d69bc4017999e6ef45e83f66b13529
Prasana Balaji D <Prasanabalaji.d@kryptosinfosys.com>
2025-06-11 11:15:15 +0000

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -122,11 +122,8 @@ app.MapControllers();
 app.Run();
 static IConfigurationRoot ConfigureWebApiAppSettings()
 {
-    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
     var configurationBuilder = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
         .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-        .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true);
-
     return configurationBuilder.Build();
 }
